70f772efd634b45aa0dc94b377b7f0d01e47aaa6
root <root@libinlxp01.ohsu.edu>
2017-01-06 12:15:50 -0800

    force remove files to prevent errors

--- import.sh
+++ import.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-rm xml/*.xml
+cd /library/solr/data/blacklight/oai-solr-harvest
+rm -f xml/*.xml
 
 ./oai.pl
+./solr_deleteall.sh
 
 echo '' > post.`date +"%Y%m%d"`.log
 
 for x in `ls xml | grep xml`
 
 do
-	java -Durl=http://localhost:8080/solr/blacklight/update -jar post.jar  xml/$x &>> post.`date +"%Y%m%d"`.log 
+	java -Durl=http://localhost:8983/solr/blacklight/update -jar post.jar  xml/$x &>> post.`date +"%Y%m%d"`.log 
 echo "processing $x"
 done
